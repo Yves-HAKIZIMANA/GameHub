755ed28eeff2a04593f941891c46f3a6ce2e163a
Yves <hkzmnyves1@gmail.com>
2023-06-09 05:58:43 +0200

    Refactoring by removing unwanted styles

--- game-hub/src/Components/GameGrid.tsx
+++ game-hub/src/Components/GameGrid.tsx
@@ -4,6 +4,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 interface Game {
   id: number;
@@ -17,14 +18,21 @@ interface FetchGamesResponse {
 
 const GameGrid = () => {
   const { games, errors, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       {errors && <Text>{errors}</Text>}
-      <SimpleGrid columns={{sm: 3, md: 1, lg: 3, xl: 5 }} spacing={10}>
-        {isLoading && skeletons.map((skeleton) => <GameCardSkeleton />)}
+      <SimpleGrid columns={{ sm: 3, md: 1, lg: 3, xl: 5 }} spacing={10}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
